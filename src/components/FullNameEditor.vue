469956ad367fa9e87d8c8e2781e9d5b4ad0e4780
MartinPucan <pucan.m@seznam.cz>
2020-09-18 21:08:17 +0200

    create fullNameEditor

---
+++ src/components/FullNameEditor.vue
@@ -0,0 +1,37 @@
+<template>
+	<div>
+		<p>{{ firstName }} {{ lastName}}</p>
+		<p>
+			<label>First name<br>
+				<input
+						type="text"
+						v-model="firstName"
+				>
+			</label>
+		</p>
+		<p>
+			<label>Last name<br>
+				<input
+						type="text"
+						v-model="lastName"
+				>
+			</label>
+		</p>
+	</div>
+</template>
+
+<script>
+export default {
+	name: "FullNameEditor",
+	data () {
+		return {
+			firstName: "Martin",
+			lastName: "Pucan"
+		}
+	}
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/index.vue
@@ -0,0 +1,31 @@
+<template>
+	<div class="wrapper">
+		<p>{{ fullName }}</p>
+		<p>
+			<label>Full name<br>
+				<input
+						type="text"
+						v-model="fullName"
+				>
+			</label>
+		</p>
+		<FullNameEditor />
+	</div>
+</template>
+
+<script>
+
+import FullNameEditor from "@/components/FullNameEditor";
+export default {
+	components: { FullNameEditor },
+	data () {
+		return {
+			fullName: "CoSiZac"
+		}
+	}
+}
+</script>
+
+<style scoped>
+
+</style>
