cdbf64c56de67c94788dac6ae58b5f2715ad4b95
MartinPucan <pucan.m@seznam.cz>
2020-09-21 00:49:31 +0200

    clean up

--- src/components/index.vue
+++ src/components/index.vue
@@ -34,30 +34,4 @@ export default {
 
 <style scoped>
 
-.alert {
-	max-width: 550px;
-	position:  relative;
-	padding: 10px 20px 10px 10px;
-	margin:  10px auto;
-	border-radius: 10px;
-	background-color: #faf8df;
-	border: 2px solid #f5c340;
-	border-left-width: 40px;
-}
-.alert:before {
-	content: '\f071';
-	/*position: absolute;*/
-	left: -27px;
-	top: 50%;
-	transform: translateY(-50%);
-	color: #fff;
-}
-.alert .close {
-	position:  absolute;
-	right: 8px;
-	cursor:  pointer;
-	top: 10px;
-	color: #999;
-}
-
 </style>
